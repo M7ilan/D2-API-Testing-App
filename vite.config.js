86bc27fa6ed5c0e465ae154b161951b70609c594
M7ilan <distny2004@gmail.com>
2023-08-13 09:24:32 +0300

    comment mkcert package

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
+// import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-	plugins: [react(), mkcert()],
-	server: { https: true },
+	plugins: [react()/*, mkcert()*/],
+	// server: { https: true },
 	base: "/D2-API-Testing-App/",
 });
