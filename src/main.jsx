51843a28f85ea49b6095ad48a2df7dfed7dbc1b2
M7ilan <distny2004@gmail.com>
2023-08-13 09:25:49 +0300

    add a checker for env

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { checkAndRefreshToken, TOKEN_REFRESH_TIME } from "./API/Auth/AuthService
 
 checkAndRefreshToken();
 setInterval(checkAndRefreshToken, TOKEN_REFRESH_TIME);
+console.log(import.meta.env.VITE_CLIENT_ID);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
