d4c4b3a20c4ce8cfa1e696ddcf67153daa251f16
OmarAfet <OmarAlenezy2004@gmail.com>
2023-08-16 17:22:27 +0300

    removing the container other then loading itself

--- src/Hooks/setLoading.jsx
+++ src/Hooks/setLoading.jsx
@@ -3,19 +3,17 @@ export const removeLoading = () => {
 	if (loading) {
 		loading.classList.add("loading-hidden");
 		setTimeout(() => {
-			loading.remove();
-			console.log("remove loading");
+			loading.parentElement.remove();
 		}, 300);
 	}
 };
 
 export const addLoading = () => {
-	if (document.getElementById("loading")) {
+	if (document.getElementById("loading-container")) {
 		return;
 	}
-	const loadingSection = document.getElementById("loading-section");
-	if (loadingSection) {
-		loadingSection.innerHTML += `<div id="loading" class="absolute opacity-100 flex bg-OpenColor-gray-0 w-full h-[calc(100vh-72px)] items-center justify-center text-4xl font-bold" style="transition: opacity 300ms">Loading...</div>`;
+	const loadingContainer = document.getElementById("loading-container");
+	if (loadingContainer) {
+		loadingContainer.innerHTML += `<div id="loading" class="absolute opacity-100 flex bg-OpenColor-gray-0 w-full h-[calc(100vh-72px)] items-center justify-center text-4xl font-bold" style="transition: opacity 300ms">Loading...</div>`;
 	}
-	console.log("add loading");
 };
