446d9a9146fd19edb2d669959e2950fc4561379f
M7ilan <distny2004@gmail.com>
2023-08-18 08:32:13 +0300

    take care of phones

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const logout = () => {
@@ -11,9 +12,11 @@ const logout = () => {
 };
 
 const NavBar = () => {
+	const [openMenu, setOpenMenu] = useState(false);
+
 	return (
 		<>
-			<div className="flex flex-wrap center justify-between items-center bg-OpenColor-gray-1 gap-4 px-8 py-2">
+			<div className="md:flex hidden flex-wrap center justify-between items-center bg-OpenColor-gray-1 gap-4 px-8 py-2">
 				<div className="flex gap-4">
 					<Link className="btn-hover" to="/D2-API-Testing-App">
 						Home
@@ -36,6 +39,42 @@ const NavBar = () => {
 						<Link className="btn-hover" to="/D2-API-Testing-App/Login">
 							Login
 						</Link>
+					)}
+				</div>
+			</div>
+			<div className="flex md:hidden flex-wrap center justify-between items-center bg-OpenColor-gray-1 gap-4 px-8 py-2">
+				<div>
+					<svg
+						onClick={() => {
+							setOpenMenu(!openMenu);
+						}}
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={2}
+						stroke="currentColor"
+						className="w-12 h-12"
+					>
+						<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+					</svg>
+					{openMenu && (
+						<div className="absolute bg-OpenColor-gray-2 shadow-lg p-2 rounded-lg flex flex-col">
+							<Link className="btn-hover hover:bg-OpenColor-gray-3" to="/D2-API-Testing-App">
+								Home
+							</Link>
+							<Link className="btn-hover hover:bg-OpenColor-gray-3" to="/D2-API-Testing-App/Search">
+								Search
+							</Link>
+							{localStorage.getItem("Logged") == "true" ? (
+								<button className="btn-hover hover:bg-OpenColor-gray-3" onClick={logout}>
+									Logout
+								</button>
+							) : (
+								<Link className="btn-hover hover:bg-OpenColor-gray-3" to="/D2-API-Testing-App/Login">
+									Login
+								</Link>
+							)}
+						</div>
 					)}
 				</div>
 			</div>
