da1d7afb0b80db361947d9ce5e6fed279dd1b8cc
M7ilan <distny2004@gmail.com>
2023-08-19 13:53:42 +0300

    Enhanced NavBar design

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const logout = () => {
 	if (localStorage.getItem("Logged") == "true") {
 		localStorage.removeItem("Auth");
-		localStorage.setItem("Logged", false);
+		localStorage.setItem("Logged", "false");
 		window.location.href = "/D2-API-Testing-App/Login";
 	} else {
 		window.location.href = "/D2-API-Testing-App/Login";
@@ -42,6 +42,7 @@ const NavBar = () => {
 					)}
 				</div>
 			</div>
+			{/* Phone Design */}
 			<div className="flex md:hidden flex-wrap center justify-between items-center bg-OpenColor-gray-1 gap-4 px-8 py-2">
 				<div>
 					<svg
@@ -58,11 +59,23 @@ const NavBar = () => {
 						<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 					</svg>
 					{openMenu && (
-						<div className="absolute bg-OpenColor-gray-2 shadow-lg p-2 rounded-lg flex flex-col">
-							<Link className="btn-hover hover:bg-OpenColor-gray-3" to="/D2-API-Testing-App">
+						<div className="absolute bg-OpenColor-gray-2 shadow-lg p-2 rounded-lg flex flex-col z-50">
+							<Link
+								onClick={() => {
+									setOpenMenu(!openMenu);
+								}}
+								className="btn-hover hover:bg-OpenColor-gray-3"
+								to="/D2-API-Testing-App"
+							>
 								Home
 							</Link>
-							<Link className="btn-hover hover:bg-OpenColor-gray-3" to="/D2-API-Testing-App/Search">
+							<Link
+								onClick={() => {
+									setOpenMenu(!openMenu);
+								}}
+								className="btn-hover hover:bg-OpenColor-gray-3"
+								to="/D2-API-Testing-App/Search"
+							>
 								Search
 							</Link>
 							{localStorage.getItem("Logged") == "true" ? (
@@ -70,7 +83,13 @@ const NavBar = () => {
 									Logout
 								</button>
 							) : (
-								<Link className="btn-hover hover:bg-OpenColor-gray-3" to="/D2-API-Testing-App/Login">
+								<Link
+									onClick={() => {
+										setOpenMenu(!openMenu);
+									}}
+									className="btn-hover hover:bg-OpenColor-gray-3"
+									to="/D2-API-Testing-App/Login"
+								>
 									Login
 								</Link>
 							)}
